fe32b465460b7137b84439e7b758a6bdd3387247
coder-aldz <coder-aldz@localhost.com>
2025-04-11 10:13:37 +0800

    Remove Main2 and Revise isMgcSqr

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,47 @@ public class Main {
 
     public static void main(String [] args){
 
-        int [][] myAray = {{1,1,1},{1,1,1},{1,1,1}}; //00 + 11 + 22 | 02 + 11 + 20
-        isMgcSqr2(myAray);
+        int [][] myArray = {{1,1,1},{1,1,1},{1,1,1}}; //00 + 11 + 22 | 02 + 11 + 20
+        isMgcSqr(myArray);
     }
 
-    public static void isMgcSqr2(int[][] theArray){
-        int[] rowTotal = new int[theArray.length];
-        int[] colTotal = new int[theArray.length];
-        int dia1 = 0;
-        int dia2 = 0;
+    public static void isMgcSqr(int[][] theArray){
+
+        int diagonal_1, diagonal_2;
+        diagonal_1 = diagonal_2 = 0;
+
+        int initialSum = 0;
+
         boolean notMagic = false;
-        for(int row = 0 ; row < theArray.length ; row++){
-            for(int col = 0 ; col < theArray.length ; col++){
-                rowTotal[row] += theArray[row][col];
-                colTotal[col] += theArray[row][col];
-            }
-            dia1 += theArray[row][row];
-            dia2 += theArray[row][theArray.length-1-row];
+
+        for(int i=0 ; i < theArray.length ; i++){
+            initialSum += theArray[0][i];
         }
-        for(int i = 0 ; i < rowTotal.length ; i++){
-            if(rowTotal[0]!=rowTotal[i] || rowTotal[0]!=colTotal[i] || rowTotal[0]!=dia1 || rowTotal[0]!=dia2){
+
+        for(int row = 0 ; row < theArray.length ; row++){
+
+            int rowTotal, colTotal;
+            rowTotal = colTotal = 0;
+
+            for(int col = 0 ; col < theArray.length ; col++){
+                rowTotal += theArray[row][col];
+                colTotal += theArray[col][row];
+            }
+            diagonal_1 += theArray[row][row];
+            diagonal_2 += theArray[row][theArray.length-1-row];
+
+            if(rowTotal!=initialSum || colTotal!=initialSum){
                 System.out.println("Sorry, not a Magic Square");
                 notMagic=true;
                 break;
             }
         }
+
+        if(diagonal_1!=initialSum || diagonal_2!=initialSum){
+            System.out.println("Sorry, not a Magic Square");
+            notMagic=true;
+        }
+
         if(!notMagic){
             System.out.println("Great! It is a Magic Square");
         }
